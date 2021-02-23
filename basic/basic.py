12927d0ea50c3dc606c4f9b177d7382133353205
Noppawit First <44127891+cutiecatz@users.noreply.github.com>
2021-02-23 17:43:40 +0700

    add code

--- basic/basic.py
+++ basic/basic.py
@@ -8,7 +8,7 @@ cv2.imshow('Original', img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Gray', gray)
 
-# Converting to grayscale
+# Converting to HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 cv2.imshow('hsv', hsv)
 
@@ -17,7 +17,7 @@ blur = cv2.GaussianBlur(img, (7, 7), 0)
 cv2.imshow('Blur', blur)
 
 # Edge Cascade
-canny = cv2.Canny(blur, 125, 175)
+canny = cv2.Canny(blur, 125, 255)
 cv2.imshow('Canny Edges', canny)
 
 # thresh
